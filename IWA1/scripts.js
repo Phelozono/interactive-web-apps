f9006f1d5528cced400511158d657dc8a33ab32c
Nosiphelo <n.zono@lab.clicklearning.org>
2023-05-03 21:40:43 +0200

    commit

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,15 +1,40 @@
-const number = document.querySelector ('[data-key= "number"]')
-const substract= document.querySelector ('[data-key="substract"]')
-const add= document.querySelector ('[data-key= "add"]')
+const MAX_NUMBER = 15
+const MIN_NUMBER = -5
+const STEP_AMOUNT = 5;
 
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
 
-const substractHandler = () => {
-counter.value= number.value;}
+console.log(typeof 123, typeof "123", typeof true)
 
-    const addHandler = () => {
-        counter.value= number.value;}
+const subtractHandler = () => {
+    const newValue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newValue
+}
 
-    substract.addEventListener('click',substractHandler)    
-    add.addEventListener('click',addHandler)    
+if (number.value.disabled === true) {
+    add.disabled = false
+}
+
+if (newValue <= MIN_NUMBER) {
+    subtract.disabled = true
+}
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) + STEP_AMOUNT
+    number.value = newValue
+}
+
+if (subtract.disabled === true) {
+    subtract.disabled = false
+}
+
+if(newValue >= MAX_NUMBER) {
+    add.disabled = false
+}
+
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
 
         
