8e9b4400e99ef1c91a2cca951ef5e767ef0eba04
Nosiphelo <n.zono@lab.clicklearning.org>
2023-05-05 22:12:05 +0200

    commit

---
+++ IWA5/challenge1.js
@@ -0,0 +1,36 @@
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+let NONE_SELECTED = '0'
+
+if (location = RSA) { shipping === 400 && currency === 'R' }
+
+if (location = NAM) {shipping === 600 && currency=== '$'}
+else (shipping = $800)
+
+shoes = 300 * 1
+toys - 100 * 5
+shirts = 150 * 'NONE_SELECTED'
+batteries= 35 * 2
+pens = 5 * 'NONE_SELECTED' 
+
+shipping = null
+currency = $
+
+if (shoes + batteries + pens + shirts > 1000) {
+	if (location = NAM && customers < 2) {
+			if (location = RSA) {
+		    shipping = 0 || calcShipping
+		}
+	}
+}
+
+if(shipping === 0 && customers !== 1 ){
+	 console.log('FREE_WARNING') }
+
+if (location = 'NK') 
+console.log('banned warning') 
+console.log('price, currency, shoes + batteries + pens + shirts + shipping')
+
+let customers = '1'
+let location = 'RSA'
+let currency = null
