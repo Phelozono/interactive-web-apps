ed2319806f01e20f6d238d3bb2af2ca486265055
Nosiphelo <n.zono@lab.clicklearning.org>
2023-05-16 13:40:58 +0200

    commit

--- IWA7/challenge3.js
+++ IWA7/challenge3.js
@@ -1,19 +1,19 @@
 const leoName = 'Leo'
 const leoSurname = 'Musvaire     '
-const leoBalance = '9394'
+const leoBalance = '-9394'
 
 const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '4582.21000111'
+const sarahBalance = '-4582.21000111'
 
 const divider = '----------------------------------'
 
 // Only change below this line
 
 
-const owed = parseFloat(leoBalance) + parseFloat(sarahBalance);
-const leo = `${leoName} ${leoSurname} Owed R${parseFloat(sarahBalance).toFixed(2)}`;
-const sarah = `${sarahName} ${sarahSurname} Owed R${parseFloat(sarahBalance).toFixed(2)}`;
+const owed = parseFloat (Math.abs(leoBalance)) + parseFloat(Math.abs(sarahBalance));
+const leo = `${leoName} ${leoSurname} Owed R${parseFloat (Math.abs(sarahBalance)).toFixed(2)}`;
+const sarah = `${sarahName} ${sarahSurname} Owed R${parseFloat(Math.abs(sarahBalance)).toFixed(2)}`;
 const total = "Total amount owed: R" + owed.toFixed(2);
-const result = "\n" + leo + "\n" + sarah + "\n" + divider + "\n" + divider + "\n  " + total.padStart(42);
+const result = "\n" + leo + "\n" + sarah  + "\n" + divider + "\n " + total + "\n" + divider;
 console.log(result);
